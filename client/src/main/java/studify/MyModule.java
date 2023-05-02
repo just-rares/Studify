b3acd9b2752005939bd70382a8e940ad073cd1a9
Razvan Gherasa <razvan_gherasa@yahoo.com>
2023-05-02 19:12:09 +0300

    Added MyModule class to configure and bind dependencies

---
+++ client/src/main/java/studify/MyModule.java
@@ -0,0 +1,18 @@
+package studify;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import studify.controller.AdminCtrl;
+import studify.controller.MainCtrl;
+import studify.controller.RegisterCtrl;
+
+public class MyModule implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(MainCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(AdminCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(RegisterCtrl.class).in(Scopes.SINGLETON);
+    }
+}
