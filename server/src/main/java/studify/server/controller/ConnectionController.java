7b4485bbbc9874724a688ba2b85827c56c6dec68
Razvan Gherasa <razvan_gherasa@yahoo.com>
2023-05-04 16:53:35 +0300

    Controller for the websocket connection with the server

--- server/src/main/java/studify/server/controller/ConnectionController.java
+++ server/src/main/java/studify/server/controller/ConnectionController.java
@@ -1,2 +1,19 @@
-package studify.server.controller;public class ConnectionController {
+package studify.server.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/connect")
+public class ConnectionController {
+    /**
+     * Checks if client is connected to the correct server url
+     * @return 200 response
+     */
+    @GetMapping()
+    public ResponseEntity connect() {
+        return ResponseEntity.ok().build();
+    }
 }
